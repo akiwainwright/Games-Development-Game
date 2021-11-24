b0105e111eb345e26d927fab004029113060808c
Aki <shibata.aki.wainwright@gmail.com>
2021-11-25 03:57:31 +0900

    Basic panels implemented

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -8,6 +8,8 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField]private GameObject m_Panels;
     private GameObject[] m_AllPanels;
 
+    private Animator m_Animator;
+
     [SerializeField]private Transform m_PlayerCamera;
 
     [Header("Panel Info")]
@@ -18,9 +20,9 @@ public class CharacterMovement : MonoBehaviour
 
     private Rigidbody m_RB;
 
-    private int m_PlayerPanelLocation;
+    private Direction m_Direction;
 
-    private int m_Direction;
+    private int m_PlayerPanelLocation;
     private int m_SpacesToMove;
 
     public float moveSpeed = 0.5f;
@@ -31,6 +33,7 @@ public class CharacterMovement : MonoBehaviour
     private void Start()
     {
         m_RB = GetComponent<Rigidbody>();
+        m_Animator = GetComponent<Animator>();
 
         //Putting panels into an array so player can navigate based on panel index
         m_AllPanels = new GameObject[m_Panels.transform.childCount];
@@ -45,7 +48,7 @@ public class CharacterMovement : MonoBehaviour
 
         //Setting up initial player values
         m_PlayerPanelLocation = 0;
-        m_Direction = 1;
+        m_Direction = Direction.Forwards;
         Vector3 startPos = new Vector3(75, 5, 97.5f);
     }
 
@@ -54,29 +57,7 @@ public class CharacterMovement : MonoBehaviour
         //If condition to stop any other input happening to player while moving
         if (!m_isMoving)
         {
-            //Determine moving and facing direction from input
-            #region Determine Movement Direction
-            //if (Input.GetKeyDown(KeyCode.W) && m_playerPanelLocation < allPanels.Length - 1)
-            //{
-            //    m_direction = 1;
-            //    m_isMoving = true;
-            //    m_nextPanel = allPanels[m_playerPanelLocation + 1].transform.position;
-            //    m_nextLocation = new Vector3(m_nextPanel.x, gameObject.transform.position.y, m_nextPanel.z);
-
-            //    //Make player face the direction they are moving
-            //    m_RB.transform.right = new Vector3((m_nextPanel.x - m_RB.position.x), 0, (m_nextPanel.z - m_RB.position.z)).normalized;
-            //}
-            //else if (Input.GetKeyDown(KeyCode.S) && m_playerPanelLocation > 0)
-            //{
-            //    m_direction = -1;
-            //    m_isMoving = true;
-            //    m_nextPanel = allPanels[m_playerPanelLocation - 1].transform.position;
-            //    m_nextLocation = new Vector3(m_nextPanel.x, gameObject.transform.position.y, m_nextPanel.z);
-
-            //    //Make player face the direction they are moving
-            //    m_RB.transform.right = new Vector3((m_nextPanel.x - m_RB.position.x), 0, (m_nextPanel.z - m_RB.position.z)).normalized;
-            //}
-            #endregion
+            m_Animator.SetBool("Moving", false);
 
             #region Moving with random number to emulate dice
 
@@ -88,31 +69,8 @@ public class CharacterMovement : MonoBehaviour
                 Debug.Log("You rolled a: " + m_SpacesToMove);
             }
 
-            if(m_SpacesToMove > 0)
-            {
-                m_Direction = 1;
-
-                //Checking if player can continue to move forwards
-                if (m_PlayerPanelLocation < m_AllPanels.Length)
-                {
-                    m_isMoving = true;
-                    m_NextPanel = m_AllPanels[m_PlayerPanelLocation].transform.position;
-                    m_NextPanel = new Vector3(m_NextPanel.x - 2.5f, m_NextPanel.y, m_NextPanel.z - 2.5f);
-                    m_NextLocation = new Vector3(m_NextPanel.x, gameObject.transform.position.y, m_NextPanel.z);
-                }
-                else
-                {
-                    Debug.Log("Can't move anymore spaces");
-                    Debug.Log("Stopped at panel " + (m_PlayerPanelLocation));
-                    m_SpacesToMove = 0;
-                    m_isMoving = false;
-                }
-            }
-            else
-            {
-                m_FacingDirection = Vector3.ProjectOnPlane(m_PlayerCamera.position - transform.position, Vector3.up).normalized;
-                //m_RB.transform.rotation = Quaternion.LookRotation(m_FacingDirection);
-            }
+            SetPlayerToMove();
+           
             #endregion
         }
 
@@ -129,17 +87,127 @@ public class CharacterMovement : MonoBehaviour
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, faceDir, m_turnSpeed);
             }
 
-            Debug.Log("Moving");
             m_RB.position = Vector3.MoveTowards(m_RB.position, m_NextLocation, moveSpeed * Time.deltaTime);
 
             if (m_RB.position == m_NextLocation)
             {
                 m_isMoving = false;
                 m_SpacesToMove--;
-                m_PlayerPanelLocation += m_Direction;
-                Debug.Log("Now at panel: " + m_PlayerPanelLocation);
+
+                //Updating the players location
+                if (m_Direction == Direction.Forwards)
+                {
+                    m_PlayerPanelLocation++;
+                }
+                else
+                {
+                    m_PlayerPanelLocation--;
+                }
             }     
         }
         #endregion
+    }
+
+    private void SetPlayerToMove()
+    {
+        if (m_SpacesToMove > 0)
+        {
+
+            //Checking if player can continue to move forwards
+            if (m_PlayerPanelLocation < m_AllPanels.Length)
+            {
+                m_isMoving = true;
+                m_Animator.SetBool("Moving", true);
+
+                //Determinig the next panel the player moves to based on the direction they need to go
+                if (m_Direction == Direction.Forwards)
+                {
+                    m_NextPanel = m_AllPanels[m_PlayerPanelLocation].transform.position;
+                }
+                else
+                {
+                    m_NextPanel = m_AllPanels[m_PlayerPanelLocation - 2].transform.position;
+                }
+                m_NextLocation = new Vector3(m_NextPanel.x, gameObject.transform.position.y, m_NextPanel.z);
+            }
+            else
+            {
+                //Stopping player if they are no more panels to progress on
+
+                Debug.Log("Can't move anymore spaces");
+                Debug.Log("Stopped at panel " + (m_PlayerPanelLocation));
+                m_SpacesToMove = 0;
+                m_isMoving = false;
+            }
+        }
+        else
+        {
+            if (m_PlayerPanelLocation > 1)
+            {
+                PanelCheck(m_AllPanels[m_PlayerPanelLocation - 1]);
+            }
+
+            Debug.Log("Now at panel " + m_PlayerPanelLocation);
+
+            //Setting the player to face forwards relative to the board once they have stopped moving
+            if (m_PlayerPanelLocation < m_AllPanels.Length)
+            {
+                m_FacingDirection = Vector3.ProjectOnPlane(m_AllPanels[m_PlayerPanelLocation].transform.position - transform.position, Vector3.up).normalized;
+            }
+            Quaternion faceDirection = Quaternion.LookRotation(m_FacingDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, faceDirection, m_turnSpeed);
+        }
+
+    }
+
+    /// <summary>
+    /// Check the panel the player has stopped on
+    /// </summary>
+    /// <param name="panel">Current panel player is located on</param>
+    private void PanelCheck(GameObject panel)
+    {
+        switch (panel.tag)
+        {
+            case "Plus3":
+                m_SpacesToMove += 3;
+                m_Direction = Direction.Forwards;
+                break;
+            case "Plus4":
+                m_SpacesToMove += 4;
+                m_Direction = Direction.Forwards;
+                break;
+            case "Plus5":
+                m_SpacesToMove += 5;
+                m_Direction = Direction.Forwards;
+                break;
+            case "Minus1":
+                m_SpacesToMove += 1;
+                m_Direction = Direction.Backwards;
+                break;
+            case "Minus2":
+                m_SpacesToMove += 2;
+                m_Direction = Direction.Backwards;
+                break;
+            case "Minus4":
+                m_SpacesToMove += 4;
+                m_Direction = Direction.Backwards;
+                break;
+            case "Minus6":
+                m_SpacesToMove += 6;
+                m_Direction = Direction.Backwards;
+                break;
+            default:
+                m_Direction = Direction.Forwards;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Possible facing directions for the player
+    /// </summary>
+    public enum Direction
+    {
+        Forwards,
+        Backwards
     }
 }
